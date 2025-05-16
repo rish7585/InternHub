d24fce081166abbb231d6afbee43ae387be6deb6
rish7585 <71748871+rish7585@users.noreply.github.com>
2025-05-16 13:09:49 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ThankYou from './ThankYou';
 const API_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:5001/api/waitlist'
-    : 'https://api.internhub.us';
+    : 'https://api.internhub.us/api/waitlist';
 
 function HomePage() {
   const navigate = useNavigate();
